59bb7329fdfa097a0fcae9e69629cdbacc80289b
harol <haroldgaelcardenastrejo@gmail.com>
2025-09-19 00:39:21 -0600

    sincronizacion

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,12 +4,5 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
-    //
-    // Bienvenido a la rama main.
-    //
-    //
-    // Bienvenido a la rama main.
-    //
-    //
+    // Listo.
 }
