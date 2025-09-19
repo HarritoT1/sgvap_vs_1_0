cbd40a5cc6f5d183ba028973dcd14a001e9f1df1
harol <haroldgaelcardenastrejo@gmail.com>
2025-09-19 00:38:44 -0600

    sdf

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,10 +5,7 @@ namespace App\Http\Controllers;
 abstract class Controller
 {
     //
-<<<<<<< HEAD
     //
-=======
->>>>>>> e266699 (bienvenida a ramas -----)
     // Bienvenido a la rama main.
     //
     //
