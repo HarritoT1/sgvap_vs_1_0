d5c849f814b0be38ff9095b78f5e76f20935228d
harol <haroldgaelcardenastrejo@gmail.com>
2025-09-23 11:24:09 -0600

    creando modal generico

--- resources/views/gp_consulta_act_filtro.blade.php
+++ resources/views/gp_consulta_act_filtro.blade.php
@@ -1,8 +1,30 @@
-
 @extends('layout')
 
 @section('content')
+    <div class="modal modal-sheet position-relative d-block p-4 py-md-5 bg-body-secondary" tabindex="-1" role="dialog"
+        id="modalmain" style="min-height: 100vh; z-index: 0; box-sizing: border-box;">
+        <div class="px-2" role="document"
+            style="width: 30rem; max-width: 100%; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); box-sizing: border-box;">
+            <div class="modal-content rounded-4 shadow" style="max-width: 100%; box-sizing: border-box;">
+                <div class="modal-header p-5 pb-4 border-bottom-0" style="max-width: 100%; box-sizing: border-box;">
+                    <h1 class="fw-bold mb-0" style="font-size: 1.2rem;">Ingresa el id del proyecto:</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
 
-
-
+                <div class="modal-body p-5 pt-0" style="max-width: 100%;">
+                    <form class="">
+                        <div class="form-floating mb-3" style="max-width: 100%;">
+                            <input type="text" class="form-control rounded-3" id="floatingInput" placeholder="">
+                            <label for="floatingInput">id</label>
+                        </div>
+                        <button class="button-custom d-block mb-2 btn btn-lg rounded-3 btn-primary" type="submit"
+                            style="background-color: var(--botones-color); font-size: 1.2rem;">Consultar</button>
+                        <small class="d-block mx-auto my-2 text-center text-body-secondary"
+                            style="font-size: 1.2rem">¡¡¡Ingrese el id sin espacios!!!</small>
+                        <hr class="mt-4">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
