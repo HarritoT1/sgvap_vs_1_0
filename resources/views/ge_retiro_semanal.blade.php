babfaab5c71f73a0f748d447dbadf1a3681697dd
Harol Gael Cardenas Trejo <haroldgaelcardenastrejo@gmail.com>
2025-09-28 15:51:50 -0600

    vista RETIRO SEMANAL TERMINADA

--- resources/views/ge_retiro_semanal.blade.php
+++ resources/views/ge_retiro_semanal.blade.php
@@ -8,176 +8,197 @@
             <table class="table table-striped table-sm">
                 <thead>
                     <tr class="text-center">
-                        <th scope="col" style="font-size: 1.4rem;">Día</th>
-                        <th scope="col" style="font-size: 1.4rem;">Desayuno</th>
-                        <th scope="col" style="font-size: 1.4rem;">Comida</th>
-                        <th scope="col" style="font-size: 1.4rem;">Cena</th>
-                        <th scope="col" style="font-size: 1.4rem;">Traslado local</th>
-                        <th scope="col" style="font-size: 1.4rem;">Traslado externo</th>
-                        <th scope="col" style="font-size: 1.4rem;">Comisión bancaria</th>
-                        <th scope="col" style="font-size: 1.4rem;">Proyecto relación</th>
+                        <th scope="col" style="font-size: 1.3rem;">Día</th>
+                        <th scope="col" style="font-size: 1.3rem;">Desayuno</th>
+                        <th scope="col" style="font-size: 1.3rem;">Comida</th>
+                        <th scope="col" style="font-size: 1.3rem;">Cena</th>
+                        <th scope="col" style="font-size: 1.3rem;">Traslado local</th>
+                        <th scope="col" style="font-size: 1.3rem;">Traslado externo</th>
+                        <th scope="col" style="font-size: 1.3rem;">Comisión bancaria</th>
+                        <th scope="col" style="font-size: 1.3rem;">id de proyecto</th>
                     </tr>
                 </thead>
                 <tbody>
+                    <tr class="text-center" style="font-size: 1.2rem;">
+                        <td>13/01/2025</td>
+                        <td>$ 90</td>
+                        <td>$ 80</td>
+                        <td>$ 70</td>
+                        <td>$ 80</td>
+                        <td>$ 120</td>
+                        <td>$ 340</td>
+                        <td>FDA4-25FE-3A1P</td>
+                    </tr>
+                    <tr class="text-center" style="font-size: 1.2rem;">
+                        <td>13/01/2025</td>
+                        <td>$ 90</td>
+                        <td>$ 80</td>
+                        <td>$ 70</td>
+                        <td>$ 80</td>
+                        <td>$ 120</td>
+                        <td>$ 340</td>
+                        <td>FDA4-25FE-3A1P</td>
+                    </tr>
+                    <tr class="text-center" style="font-size: 1.2rem;">
+                        <td>13/01/2025</td>
+                        <td>$ 90</td>
+                        <td>$ 80</td>
+                        <td>$ 70</td>
+                        <td>$ 80</td>
+                        <td>$ 120</td>
+                        <td>$ 340</td>
+                        <td>FDA4-25FE-3A1P</td>
+                    </tr>
+                    <tr class="text-center" style="font-size: 1.2rem;">
+                        <td>13/01/2025</td>
+                        <td>$ 90</td>
+                        <td>$ 80</td>
+                        <td>$ 70</td>
+                        <td>$ 80</td>
+                        <td>$ 120</td>
+                        <td>$ 340</td>
+                        <td>FDA4-25FE-3A1P</td>
+                    </tr>
+                    <tr class="text-center" style="font-size: 1.2rem;">
+                        <td>13/01/2025</td>
+                        <td>$ 90</td>
+                        <td>$ 80</td>
+                        <td>$ 70</td>
+                        <td>$ 80</td>
+                        <td>$ 120</td>
+                        <td>$ 340</td>
+                        <td>FDA4-25FE-3A1P</td>
+                    </tr>
+                    <tr class="text-center" style="font-size: 1.2rem;">
+                        <td>13/01/2025</td>
+                        <td>$ 90</td>
+                        <td>$ 80</td>
+                        <td>$ 70</td>
+                        <td>$ 80</td>
+                        <td>$ 120</td>
+                        <td>$ 340</td>
+                        <td>FDA4-25FE-3A1P</td>
+                    </tr>
+                    <tr class="text-center" style="font-size: 1.2rem;">
+                        <td>13/01/2025</td>
+                        <td>$ 90</td>
+                        <td>$ 80</td>
+                        <td>$ 70</td>
+                        <td>$ 80</td>
+                        <td>$ 120</td>
+                        <td>$ 340</td>
+                        <td>FDA4-25FE-3A1P</td>
+                    </tr>
+                    <tr class="text-center" style="font-size: 1.2rem;">
+                        <td>13/01/2025</td>
+                        <td>$ 90</td>
+                        <td>$ 80</td>
+                        <td>$ 70</td>
+                        <td>$ 80</td>
+                        <td>$ 120</td>
+                        <td>$ 340</td>
+                        <td>FDA4-25FE-3A1P</td>
+                    </tr>
+                    <tr class="text-center" style="font-size: 1.2rem;">
+                        <td>13/01/2025</td>
+                        <td>$ 90</td>
+                        <td>$ 80</td>
+                        <td>$ 70</td>
+                        <td>$ 80</td>
+                        <td>$ 120</td>
+                        <td>$ 340</td>
+                        <td>FDA4-25FE-3A1P</td>
+                    </tr>
+                    <tr class="text-center" style="font-size: 1.2rem;">
+                        <td>13/01/2025</td>
+                        <td>$ 90</td>
+                        <td>$ 80</td>
+                        <td>$ 70</td>
+                        <td>$ 80</td>
+                        <td>$ 120</td>
+                        <td>$ 340</td>
+                        <td>FDA4-25FE-3A1P</td>
+                    </tr>
+                    <tr class="text-center" style="font-size: 1.2rem;">
+                        <td>13/01/2025</td>
+                        <td>$ 90</td>
+                        <td>$ 80</td>
+                        <td>$ 70</td>
+                        <td>$ 80</td>
+                        <td>$ 120</td>
+                        <td>$ 340</td>
+                        <td>FDA4-25FE-3A1P</td>
+                    </tr>
+                    <tr class="text-center" style="font-size: 1.2rem;">
+                        <td>13/01/2025</td>
+                        <td>$ 90</td>
+                        <td>$ 80</td>
+                        <td>$ 70</td>
+                        <td>$ 80</td>
+                        <td>$ 120</td>
+                        <td>$ 340</td>
+                        <td>FDA4-25FE-3A1P</td>
+                    </tr>
+                    <tr class="text-center" style="font-size: 1.2rem;">
+                        <td>13/01/2025</td>
+                        <td>$ 90</td>
+                        <td>$ 80</td>
+                        <td>$ 70</td>
+                        <td>$ 80</td>
+                        <td>$ 120</td>
+                        <td>$ 340</td>
+                        <td>FDA4-25FE-3A1P</td>
+                    </tr>
+                    <tr class="text-center" style="font-size: 1.2rem;">
+                        <td>13/01/2025</td>
+                        <td>$ 90</td>
+                        <td>$ 80</td>
+                        <td>$ 70</td>
+                        <td>$ 80</td>
+                        <td>$ 120</td>
+                        <td>$ 340</td>
+                        <td>FDA4-25FE-3A1P</td>
+                    </tr>
+                    <tr class="text-center" style="font-size: 1.2rem;">
+                        <td>13/01/2025</td>
+                        <td>$ 90</td>
+                        <td>$ 80</td>
+                        <td>$ 70</td>
+                        <td>$ 80</td>
+                        <td>$ 120</td>
+                        <td>$ 340</td>
+                        <td>FDA4-25FE-3A1P</td>
+                    </tr>
+                    <tr class="text-center" style="font-size: 1.2rem;">
+                        <td>13/01/2025</td>
+                        <td>$ 90</td>
+                        <td>$ 80</td>
+                        <td>$ 70</td>
+                        <td>$ 80</td>
+                        <td>$ 120</td>
+                        <td>$ 340</td>
+                        <td>FDA4-25FE-3A1P</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <h2 class="fw-bold my-3" style="font-size: 2rem; text-align:justify">Total de retiro semanal de: $nombre</h2>
+        <div class="table-responsive small">
+            <table class="table table-striped table-sm">
+                <thead>
                     <tr class="text-center">
-                        <td>13/01/2025</td>
-                        <td>$ 90</td>
-                        <td>$ 80</td>
-                        <td>$ 70</td>
-                        <td>$ 80</td>
-                        <td>$ 120</td>
-                        <td>$ 340</td>
-                        <td>FDA4-25FE-3A1P</td>
+                        <th scope="col" style="font-size: 1.3rem;">Total alimentos</th>
+                        <th scope="col" style="font-size: 1.3rem;">Total traslados</th>
+                        <th scope="col" style="font-size: 1.3rem;">Total comisión</th>
+                        <th scope="col" style="font-size: 1.3rem;"><em>TOTAL A RETIRAR EN ESTA SEMANA</em></th>
                     </tr>
-                    <tr class="text-center">
-                        <td>13/01/2025</td>
-                        <td>$ 90</td>
-                        <td>$ 80</td>
-                        <td>$ 70</td>
-                        <td>$ 80</td>
-                        <td>$ 120</td>
-                        <td>$ 340</td>
-                        <td>FDA4-25FE-3A1P</td>
-                    </tr>
-                    <tr class="text-center">>
-                        <td>13/01/2025</td>
-                        <td>$ 90</td>
-                        <td>$ 80</td>
-                        <td>$ 70</td>
-                        <td>$ 80</td>
-                        <td>$ 120</td>
-                        <td>$ 340</td>
-                        <td>FDA4-25FE-3A1P</td>
-                    </tr>
-                    <tr class="text-center">>
-                        <td>13/01/2025</td>
-                        <td>$ 90</td>
-                        <td>$ 80</td>
-                        <td>$ 70</td>
-                        <td>$ 80</td>
-                        <td>$ 120</td>
-                        <td>$ 340</td>
-                        <td>FDA4-25FE-3A1P</td>
-                    </tr>
-                    <tr class="text-center">>
-                        <td>13/01/2025</td>
-                        <td>$ 90</td>
-                        <td>$ 80</td>
-                        <td>$ 70</td>
-                        <td>$ 80</td>
-                        <td>$ 120</td>
-                        <td>$ 340</td>
-                        <td>FDA4-25FE-3A1P</td>
-                    </tr>
-                    <tr class="text-center">>
-                        <td>13/01/2025</td>
-                        <td>$ 90</td>
-                        <td>$ 80</td>
-                        <td>$ 70</td>
-                        <td>$ 80</td>
-                        <td>$ 120</td>
-                        <td>$ 340</td>
-                        <td>FDA4-25FE-3A1P</td>
-                    </tr>
-                    <tr class="text-center">>
-                        <td>13/01/2025</td>
-                        <td>$ 90</td>
-                        <td>$ 80</td>
-                        <td>$ 70</td>
-                        <td>$ 80</td>
-                        <td>$ 120</td>
-                        <td>$ 340</td>
-                        <td>FDA4-25FE-3A1P</td>
-                    </tr>
-                    <tr class="text-center">>
-                        <td>13/01/2025</td>
-                        <td>$ 90</td>
-                        <td>$ 80</td>
-                        <td>$ 70</td>
-                        <td>$ 80</td>
-                        <td>$ 120</td>
-                        <td>$ 340</td>
-                        <td>FDA4-25FE-3A1P</td>
-                    </tr>
-                    <tr class="text-center">>
-                        <td>13/01/2025</td>
-                        <td>$ 90</td>
-                        <td>$ 80</td>
-                        <td>$ 70</td>
-                        <td>$ 80</td>
-                        <td>$ 120</td>
-                        <td>$ 340</td>
-                        <td>FDA4-25FE-3A1P</td>
-                    </tr>
-                    <tr class="text-center">>
-                        <td>13/01/2025</td>
-                        <td>$ 90</td>
-                        <td>$ 80</td>
-                        <td>$ 70</td>
-                        <td>$ 80</td>
-                        <td>$ 120</td>
-                        <td>$ 340</td>
-                        <td>FDA4-25FE-3A1P</td>
-                    </tr>
-                    <tr class="text-center">>
-                        <td>13/01/2025</td>
-                        <td>$ 90</td>
-                        <td>$ 80</td>
-                        <td>$ 70</td>
-                        <td>$ 80</td>
-                        <td>$ 120</td>
-                        <td>$ 340</td>
-                        <td>FDA4-25FE-3A1P</td>
-                    </tr>
-                    <tr class="text-center">>
-                        <td>13/01/2025</td>
-                        <td>$ 90</td>
-                        <td>$ 80</td>
-                        <td>$ 70</td>
-                        <td>$ 80</td>
-                        <td>$ 120</td>
-                        <td>$ 340</td>
-                        <td>FDA4-25FE-3A1P</td>
-                    </tr>
-                    <tr class="text-center">>
-                        <td>13/01/2025</td>
-                        <td>$ 90</td>
-                        <td>$ 80</td>
-                        <td>$ 70</td>
-                        <td>$ 80</td>
-                        <td>$ 120</td>
-                        <td>$ 340</td>
-                        <td>FDA4-25FE-3A1P</td>
-                    </tr>
-                    <tr class="text-center">>
-                        <td>13/01/2025</td>
-                        <td>$ 90</td>
-                        <td>$ 80</td>
-                        <td>$ 70</td>
-                        <td>$ 80</td>
-                        <td>$ 120</td>
-                        <td>$ 340</td>
-                        <td>FDA4-25FE-3A1P</td>
-                    </tr>
-                    <tr class="text-center">>
-                        <td>13/01/2025</td>
-                        <td>$ 90</td>
-                        <td>$ 80</td>
-                        <td>$ 70</td>
-                        <td>$ 80</td>
-                        <td>$ 120</td>
-                        <td>$ 340</td>
-                        <td>FDA4-25FE-3A1P</td>
-                    </tr>
-                    <tr class="text-center">>
-                        <td>13/01/2025</td>
-                        <td>$ 90</td>
-                        <td>$ 80</td>
-                        <td>$ 70</td>
-                        <td>$ 80</td>
-                        <td>$ 120</td>
-                        <td>$ 340</td>
-                        <td>FDA4-25FE-3A1P</td>
+                </thead>
+                <tbody>
+                    <tr class="text-center" style="font-size: 1.2rem;">
+                        <td>$ 540</td>
+                        <td>$ 0</td>
+                        <td>$ 9.45</td>
+                        <td>$ 549.45</td>
                     </tr>
                 </tbody>
             </table>
