44848f0b66f9e41a7705f17a1f206f05e515d752
harol <haroldgaelcardenastrejo@gmail.com>
2025-10-02 09:31:03 -0600

    configurando script

--- resources/views/gdm_gasolina_alta_dispersion.blade.php
+++ resources/views/gdm_gasolina_alta_dispersion.blade.php
@@ -186,7 +186,7 @@
             <hr class="my-4 mb-2">
 
             <button class="d-block mx-auto btn btn-primary btn-lg fw-bold button-custom" type="button"
-                id="button_analizar_excel" onclick="analizar_xls()" style="background-color: rgb(161, 160, 160)"
+                id="button_analizar_excel" onclick="analizar_xls(['fecha_dispersion', 'project_id', 'vehicle_id', 'costo_lt', 'cant_litros', 'monto_dispersado', 'base_imponible', 'iva_acumulado', 'importe_total'])" style="background-color: rgb(161, 160, 160)"
                 disabled>Analizar excel y almacenar registros</button>
         </div>
     </div>
