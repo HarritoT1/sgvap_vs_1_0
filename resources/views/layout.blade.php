6030a1e5c6371e1aedf0a0c311a7f653c36c22cf
harol <haroldgaelcardenastrejo@gmail.com>
2025-10-02 09:32:22 -0600

    ultimo cambio por hoy

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -891,7 +891,7 @@ l838 0 -282 -282 c-304 -305 -305 -307 -288 -395 9 -49 69 -109 118 -118 91
                                     <ul class="flex-column vineta" style="padding-left: 75px;">
                                         <li class="nav-item">
                                             <a class="nav-link d-flex align-items-center gap-2 text-white fw-bold"
-                                                href="#" target="_self">
+                                                href="/gdm_gasolina_alta_dispersion" target="_self">
                                                 Registrar disp. gasolina.
                                             </a>
                                         </li>
