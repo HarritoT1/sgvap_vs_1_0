394bae4de81f6a303469f077892f3e106af50de3
harol <haroldgaelcardenastrejo@gmail.com>
2025-10-06 12:16:31 -0600

    terminada consulta_act graficas de gasolina

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1070,7 +1070,7 @@ l838 0 -282 -282 c-304 -305 -305 -307 -288 -395 9 -49 69 -109 118 -118 91
             if (document.getElementById("graficas_barras")) {
                 set_required_input_find_id_proyect()
             }
-            if (document.getElementById("crear_dm_gasolina")) {
+            if (document.getElementById("crear_dm_gasolina") || document.getElementById("monto_dispersado")) {
                 asig_listener_autocomputed_inputs()
             }
             if (document.getElementById("xls_gasoline")) {
--- routes/web.php
+++ routes/web.php
@@ -91,3 +91,7 @@ Route::get('/gdm_gasolina_disp_consulta_act_filtro', function () {
 Route::post('/gasolina_disp_consulta_filtro', function () {
     return response()->json([["id" => 1, "fecha_dispersion" => "2025-12-24", "project_name" => "uetamo", "vehicle_id" => "ASFG-AH4D"], ["id" => 2, "fecha_dispersion" => "2025-12-25", "project_name" => "parajilla", "vehicle_id" => "ZXCW-RT56"], ["id" => 3, "fecha_dispersion" => "2025-12-26", "project_name" => "zacatenco", "vehicle_id" => "QWER-YUI7"]]);
 });
+
+Route::get('/gdm_gasolina_disp_consulta_act', function () {
+    return view('gdm_gasolina_disp_consulta_act'); //http://127.0.0.1:8000/gdm_gasolina_disp_consulta_act
+});
