51842a69f88ca349f2d7197d16e0e13dff8641b3
harol <haroldgaelcardenastrejo@gmail.com>
2025-10-15 10:14:59 -0600

    TERMINANDO gv_consulta_act scripts_3

--- public/js/scripts_sgvap_3.js
+++ public/js/scripts_sgvap_3.js
@@ -16,15 +16,64 @@ function asig_listener_change_vehicle_foto_prev() {
     });
 }
 
-function save_values_of_inputs_with_files () {
-    const form = document.getElementById("actualizar");
-    const inputs = form.querySelectorAll("input, textarea, select");
+function save_values_of_inputs_with_files() {
+    const form = document.getElementById("actualizar_version_2");
+    const inputs = form.querySelectorAll("input:not([type='file']), textarea, select");
+    const imgs = form.querySelectorAll("img.img_file");
 
     valoresOriginales = {};
-    inputs.forEach(input => {
-        if (!(input.type === 'radio')) valoresOriginales[input.name] = input.value;
+    inputs.forEach((input, index) => {
+        if (!(input.type === 'radio') && !(input.type === 'checkbox')) valoresOriginales[input.name] = input.value;
         if (input.type === 'radio' && input.checked === true) valoresOriginales[input.name] = input.value;
+        if (input.type === 'checkbox') valoresOriginales[input.value] = input.checked;
+    });
+
+    imgs.forEach((img, index) => {
+        valoresOriginales[`ruta_evidencia_${index + 1}`] = img.src;
     });
 
     console.log(valoresOriginales);
+}
+
+function ask_before_submit_with_files() {
+    if (confirm("¿Está seguro de guardar los cambios?")) {
+        const form = document.getElementById("actualizar_version_2");
+        const inputs = form.querySelectorAll("input:not([type='file']), textarea, select");
+        const imgs = form.querySelectorAll("img.img_file");
+
+        let valoresOriginales2 ={}
+
+        const huboCambios = Array.from(inputs).some(
+            (input, index) => {
+                if (!(input.type === 'radio') && !(input.type === 'checkbox')) valoresOriginales2[input.name] = input.value;
+                if (input.type === 'radio' && input.checked === true) valoresOriginales2[input.name] = input.value;
+                if (input.type === 'checkbox') valoresOriginales2[input.value] = input.checked;
+
+                if (!(input.type === 'radio') && !(input.type === 'checkbox')) return valoresOriginales[input.name] !== input.value;
+                if (input.type === 'radio' && input.checked === true) return valoresOriginales[input.name] !== input.value;
+                if (input.type === 'checkbox') return valoresOriginales[input.value] !== input.checked;
+            }
+        );
+
+        const huboCambiossrc = Array.from(imgs).some(
+            (img, index) => {
+                valoresOriginales2[`ruta_evidencia_${index + 1}`] = img.src;
+                return valoresOriginales[`ruta_evidencia_${index + 1}`] !== img.src;
+            }
+        );
+
+        console.log(valoresOriginales2);
+
+        console.log(huboCambios);
+
+        console.log(huboCambiossrc);
+
+        if ((!huboCambios) && (!huboCambiossrc)) {
+            alert("No realizaste ningún cambio.");
+            window.location.reload();
+            return;
+        }
+
+        form.requestSubmit();
+    }
 }
