01be7a75fabc933664648b941e5697329a6337cd
harol <haroldgaelcardenastrejo@gmail.com>
2025-10-29 11:18:57 -0600

    crearemos de nuevo los moedlos

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        /*Schema::create('users', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
@@ -25,7 +25,7 @@ return new class extends Migration
             $table->string('email')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
-        });*/
+        });
 
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
