ca47d284b58c43f4b02d906e10af01024770980e
harol <haroldgaelcardenastrejo@gmail.com>
2025-10-31 00:50:30 -0600

    ajustando mensajes scripts

--- public/js/scripts_sgvap.js
+++ public/js/scripts_sgvap.js
@@ -105,7 +105,7 @@ function asig_listener_autocomplete_rfc() {
                     sugerencias.appendChild(option);
                 });
             }).catch(err => {
-                console.error("Error al buscar RFC de clientes:", err);
+                console.warn("Error al buscar RFC de empleados:", err);
             });
     });
 }
@@ -138,7 +138,7 @@ function asig_listener_autocomplete_id_proyect() {
                     sugerencias.appendChild(option);
                 });
             }).catch(err => {
-                console.error("Error al buscar RFC de clientes:", err);
+                console.warn("Error al buscar id de proyectos:", err);
             });
     });
 }
