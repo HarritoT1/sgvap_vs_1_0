5be8093cab7e6869d96e9ece7de655d546a67a20
harol <haroldgaelcardenastrejo@gmail.com>
2025-11-03 13:48:03 -0600

    QUEDA PENDIENTE CONTROLADOR DE DailyExpenseReport

---
+++ app/Http/Controllers/DailyExpenseReportController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\DailyExpenseReport;
+use App\Models\Employee;
+use App\Models\Project;
+
+class DailyExpenseReportController extends Controller
+{
+    //
+}
