4c1a643813073aabc8508fcd600cacb1037e0a82
harol <haroldgaelcardenastrejo@gmail.com>
2025-11-13 13:56:05 -0600

    ya solo falta mostrar errores de validaciones de inserciones masivas

--- app/Http/Requests/StoreManyGasolineDispersionRequest.php
+++ app/Http/Requests/StoreManyGasolineDispersionRequest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
 
-class StoreManyGasolineDispersionsRequest extends FormRequest
+class StoreManyGasolineDispersionRequest extends FormRequest
 {
     public function authorize(): bool
     {
